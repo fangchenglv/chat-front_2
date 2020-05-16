733474a298d0b1cdeef247bc3e8c46bb42087e84
张赟 <1121786974@qq.com>
2020-05-16 14:24:46 +0800

    groupFriend

--- src/router/mobile/index.js
+++ src/router/mobile/index.js
@@ -145,6 +145,15 @@ let router = new Router({
         keepAlive:false,
       }
     },
+    //查看群成员
+    {
+      path: "/checkGroupFriend/:groupId",
+      name: "checkGroupFriend",
+      component: () => import("../../components/mobile/GroupChat/checkGroupFriend"),
+      meta: {
+        keepAlive: false,
+      }
+    },
     //好友列表页
     {
       path:'/friendList',
