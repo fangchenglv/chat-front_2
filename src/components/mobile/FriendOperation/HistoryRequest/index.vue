68b897dab93f485f2f90b5ca2d667c52daed6a8f
liyuting <329631739@qq.com>
2020-05-20 18:23:12 +0800

    完成查看历史好友申请

--- src/components/mobile/FriendOperation/HistoryRequest/index.vue
+++ src/components/mobile/FriendOperation/HistoryRequest/index.vue
@@ -7,21 +7,108 @@
       @click-left="onClickLeft"
       id="head"
     />
-    
+    <van-tabbar v-model="active" @change="handleClick" class="switch">
+      <van-tabbar-item icon="agree-o" name="1" >已经同意添加的好友</van-tabbar-item>
+      <van-tabbar-item icon="refuse-o" name="2" >已经拒绝添加的好友</van-tabbar-item>
+    </van-tabbar>
+    <van-pagination v-model="currentPage" :total-items=total :items-per-page="5" @change="handleCurrentChange" class="page"/>
+
+    <div v-for="tables in tableData" :key="tables.index">
+      <van-cell
+        :title="tables.friendUserName"
+        :value="tables.applyTime"
+        :label="tables.status"
+        style="background-color:rgb(250,250,250);text-align:left;border:solid 0.02rem #ddd; top:2.3rem;border-left:none; border-right:none"/>
+    </div>
+
   </div>
 </template>
 
 <script>
+  import {searchFriendRequest} from '@/api/friendOperation';
 export default {
   name:"historyFriend",
+  data(){
+    return{
+      findList:[],
+      currentPage: 1,
+      total: 0,
+      page: 1,
+      pageSize: 5,
+      pageNum:1,
+      code:1,
+      index:0,
+      id:this.$store.getters.userId,
+      value:true,
+      friendGroupList:this.$store.getters.myFriendList,
+      tableData:[],
+      active:-1,
+    }
+  },
   methods:{
     onClickLeft(){
       this.$router.push({path:"/friendOperation"});
-    }
+    },
+    handleClick(index) {
+      this.tableData = [];
+      // console.log("第几个选项",index);
+      searchFriendRequest(this.id, index, this.pageNum, this.pageSize)
+        .then(response => {
+          let data = response.data.data;
+          // console.log("在哪一页：",this.pageNum)
+          this.index=index;
+          if (data.total !== 0) {
+            this.total = data.total;
+            this.findList = data.list;
+            for(let i = 0; i < this.findList.length; i++){
+              let tmp = {};
+              tmp['applyTime'] = this.findList[i]['applyTime'];
+              tmp['friendUserName'] = this.findList[i]['fromUser']['userName'];
+              tmp['friendNickName'] = this.findList[i]['fromUser']['nickName'];
+              tmp['status'] = (index === "1")? "通过申请" : "拒绝申请";
+              this.tableData.push(tmp);
+            }
+          }
+        })
+        .catch(error => {
+          console.log("!!", error);
+        });
+    },
+    handleCurrentChange(val) {
+      this.pageNum = val;
+      // this.handleCommand();
+      console.log(this.pageNum);
+      this.handleClick(this.index);
+    },
   }
 }
 </script>
 
 <style>
-
+  .switch{
+    background-color: #fff;
+    font-size: 0.4rem;
+    width: 100%;
+    position: absolute;
+    left: 0rem;
+    top: 1rem;
+    line-height: 1rem;
+    margin-bottom: 0.1rem;
+  }
+  /*.med{*/
+  /*  position: relative;*/
+  /*  top: 2.3rem;*/
+  /*  left: 0rem;*/
+  /*  line-height: 1rem;*/
+  /*  margin-bottom: 0rem;*/
+  /*  width: 100%;*/
+  /*}*/
+  .page{
+    width: 100%;
+    font-size: 0.4rem;
+    position: absolute;
+    left: 0rem;
+    bottom: 0rem;
+    background-color: #fff;
+  }
 </style>
