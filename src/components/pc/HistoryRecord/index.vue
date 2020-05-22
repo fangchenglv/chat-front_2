f272591a4629cf9523913b9d0a6582ed1146560b
lemon666 <929749865@qq.com>
2020-05-22 14:51:12 +0800

    pc单聊未读消息更改

--- src/components/pc/HistoryRecord/index.vue
+++ src/components/pc/HistoryRecord/index.vue
@@ -2,15 +2,15 @@
     <el-container>
         <el-main style="background-color:rgb(255,255,255)">
             <div v-for="(item, ind) in historyMessageList" :key="ind">
-                <FriendItem v-if="item.fromUser.id == userId" :img="item.fromUser.avatar" me="true" :msg="item.message" :name="item.fromUser.nickName"></FriendItem>
-                <MyItem v-else :img="item.fromUser.avatar" :msg="item.message" :name="item.fromUser.nickName"></MyItem>
+                 <FriendItem v-if="item.fromUser.id == userId" :messageid="item.id" :img="item.fromUser.avatar" me="true" :msg="item.message" :name="item.fromUser.nickName"></FriendItem>
+                 <MyItem v-else :messageid="item.id" :img="item.fromUser.avatar" :msg="item.message" :name="item.fromUser.nickName"></MyItem>
             </div>
         </el-main>
     </el-container>
 </template>
 
 <script>
-import {getHistoryReadList} from '@/api/friendOperation'
+import {getSingleHistoryReadList} from '@/api/friendOperation'
 import FriendItem from "../PrivateChatRoom/FriendItem"
 import MyItem from "../PrivateChatRoom/MyItem"
 
@@ -33,18 +33,26 @@ export default {
 
     methods:{
       getHistoryList(){
-        getHistoryReadList(this.$store.getters.userId, this.$route.params.toId).then(response =>{
-            this.historyMessageList = response.data.data;
-            // console.log("历史信息", this.historyMessageList);
+        getSingleHistoryReadList(this.$route.params.toId, this.userId).then(response =>{
+            console.log("哪个是我", this.userId)
+            let hist = response.data.data;
+            for (let i = 0; i < hist.length; i++) {
+               let t = {fromUser:{
+                  id: hist[i].fromUserId,
+                  avatar: hist[i].fromAvatar,
+                  nickName: hist[i].fromName
+                  },
+                  message: hist[i].content,
+                  id:hist[i].type
+               }
+               this.historyMessageList.push(t)
+            }
+
+            console.log("历史信息", this.historyMessageList);
         }).catch();
       },
     }
 }
 </script>
 
-<style scoped>
-/* @import "./chat.scss";
-.el-main{
-    padding: 10px;
-} */
-</style>
+
