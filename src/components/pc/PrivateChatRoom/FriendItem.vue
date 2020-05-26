a9e097aafe4f589adc9f05b5712eda65b84b82ba
lemon666 <929749865@qq.com>
2020-05-26 09:56:27 +0800

    pc好友列表以及消息头像更改

--- src/components/pc/PrivateChatRoom/FriendItem.vue
+++ src/components/pc/PrivateChatRoom/FriendItem.vue
@@ -1,7 +1,7 @@
 <template>
 <div style="float:right; width:100%; margin-bottom:0.1rem">
 
-            <div class="demo-type" style="float:right;width: 1rem;height: 1rem;">
+            <div class="demo-type" style="float:right;width: 1rem;height:1rem;">
 
               <div>
                 <el-avatar > {{name}} </el-avatar>
@@ -9,7 +9,7 @@
             </div>
 
 
-  <div v-if="messageid === 0" style="display:inline-block; font-size:0.6rem;margin-right:0.2rem;max-width:50%;word-wrap:break-word;word-break:break-all;float:inherit;text-align:left">{{msg}}</div>
+  <div v-if="messageid === 0" style="display:inline-block;font-size:0.5rem;margin-right:0.2rem;max-width:50%;word-wrap:break-word;word-break:break-all;float:inherit;text-align:left">{{msg}}</div>
   <img v-if="messageid === 1" :src="msg" alt="图片加载失败" style="display:inline-block; margin-right:0.2rem;max-width:50%;word-wrap:break-word;word-break:break-all;float:inherit;text-align:left"/>
 </div>
 </template>
--- src/components/pc/PrivateChatRoom/MyItem.vue
+++ src/components/pc/PrivateChatRoom/MyItem.vue
@@ -9,7 +9,7 @@
                     <el-avatar > {{name}} </el-avatar>
                   </div>
     </div>
-    <div v-if="messageid === 0" style="display:inline-block; font-size:0.6rem;margin-left:0.2rem;max-width:50%;word-wrap:break-word;word-break:break-all;float:inherit;text-align:left">{{messages}}</div>
+    <div v-if="messageid === 0" style="display:inline-block; font-size:0.5rem;margin-left:0.2rem;max-width:50%;word-wrap:break-word;word-break:break-all;float:inherit;text-align:left">{{messages}}</div>
     <img v-if="messageid === 1" :src="msg" alt="图片加载失败" style="display:inline-block;margin-left:0.2rem;max-width:50%;word-wrap:break-word;word-break:break-all;float:inherit;text-align:left"/>
   </div>
 
