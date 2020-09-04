e3fc98444e8efb73427e4bcdcd1f430ff008b495
lemon666 <929749865@qq.com>
2020-09-04 10:57:09 +0800

    录音功能实现

--- src/components/mobile/groupChatPage/index.vue
+++ src/components/mobile/groupChatPage/index.vue
@@ -25,6 +25,11 @@
     <!-- 聊天底部 -->
     <van-tabbar>
       <van-tabbar-item >
+
+      <div class="contrs" >
+              <van-button type="primary" plain  @click="startRecording" style="font-size:0.3rem;position: absolute;bottom: 50px;left: 100px;">点击讲话</van-button>
+              <van-button type="primary" plain  @click="sendRecording" style="font-size:0.3rem;position: absolute;bottom: 50px;left: 200px;">结束讲话</van-button>
+      </div>
         <input placeholder="输入信息" v-model="message"class="input" />
         <van-button @click="sendMsg" plain type="info" >发送</van-button>
         <div class="bottom" >
@@ -45,6 +50,8 @@
   </div>
 </template>
 
+<script src="jquery-3.2.1.min.js"></script>
+<script src="recoder.js"></script>
 <script>
 import GroupFriendItem from "../groupChatPage/groupFriendItem";
 import GroupMyItem from "../groupChatPage/groupMyItem";
@@ -65,6 +72,7 @@ export default {
       fileUrl: "",
       total:0,
       ff:0,
+
       reader:null,
       fileUpload:{
         cuLoaded: 0,
@@ -82,6 +90,9 @@ export default {
       groupId:"" + this.$route.params.groupId,
       newGroupFriend: this.$store.getters.myGroupFriends[this.$route.params.groupId],
       unreadLeaveFriend:this.$store.getters.leaveMessage,
+      audio : document.querySelector('audio'),
+      ct:"",
+      recorder:"",
     };
   },
   computed:{
@@ -97,7 +108,9 @@ export default {
     },
   },
   mounted() {
+    this.Recorder(this.recorder);
     this.init();
+
   },
   beforeDestroy(){
     this.$websocket.state.groupMessage[this.groupId] = this.currendStartChatList;
@@ -106,12 +119,305 @@ export default {
   methods:{
 
     init(){
+
       console.log("23333");
       this.$websocket.state.groupUnreadNumber[this.groupId] = null;
       this.$websocket.dispatch("StartChatId", [this.groupId, "group"]);
       this.getUnreadList(this.$store.getters.userId, this.groupId);
       this.websockOnMessage();
+
     },
+
+Recorder(recorder){
+  console.log("啥啥啥开始吗");
+    //兼容
+    window.URL = window.URL || window.webkitURL;
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    var HZRecorder = function (stream, config) {
+      console.log("这是哪里");
+        config = config || {};
+        config.sampleBits = config.sampleBits || 8;      //采样数位 8, 16
+        config.sampleRate = config.sampleRate || (44100 / 6);   //采样率(1/6 44100)
+
+
+        //创建一个音频环境对象
+        var audioContext = window.AudioContext || window.webkitAudioContext;
+        var context = new audioContext();
+
+        //将声音输入这个对像
+        var audioInput = context.createMediaStreamSource(stream);
+
+        //设置音量节点
+        var volume = context.createGain();
+        audioInput.connect(volume);
+
+context.onstatechange = function() {
+  console.log("state",context.state);
+}
+        //创建缓存，用来缓存声音
+        var bufferSize = 4096;
+        // 创建声音的缓存节点，createScriptProcessor方法的
+        // 第二个和第三个参数指的是输入和输出都是双声道。
+        var recorder = context.createScriptProcessor(bufferSize, 2, 2);
+
+        var audioData = {
+            size: 0          //录音文件长度
+            , buffer: []     //录音缓存
+            , inputSampleRate: context.sampleRate    //输入采样率
+            , inputSampleBits: 16       //输入采样数位 8, 16
+            , outputSampleRate: config.sampleRate    //输出采样率
+            , oututSampleBits: config.sampleBits       //输出采样数位 8, 16
+            , input: function (data) {
+                this.buffer.push(new Float32Array(data));
+                this.size += data.length;
+            }
+            , compress: function () { //合并压缩
+                //合并
+                var data = new Float32Array(this.size);
+                var offset = 0;
+                for (var i = 0; i < this.buffer.length; i++) {
+                    data.set(this.buffer[i], offset);
+                    offset += this.buffer[i].length;
+                }
+                //压缩
+                var compression = parseInt(this.inputSampleRate / this.outputSampleRate);
+                var length = data.length / compression;
+                var result = new Float32Array(length);
+                var index = 0, j = 0;
+                while (index < length) {
+                    result[index] = data[j];
+                    j += compression;
+                    index++;
+                }
+                return result;
+            }
+            , encodeWAV: function () {
+                var sampleRate = Math.min(this.inputSampleRate, this.outputSampleRate);
+                var sampleBits = Math.min(this.inputSampleBits, this.oututSampleBits);
+                var bytes = this.compress();
+                var dataLength = bytes.length * (sampleBits / 8);
+                var buffer = new ArrayBuffer(44 + dataLength);
+                var data = new DataView(buffer);
+
+                var channelCount = 1;//单声道
+                var offset = 0;
+
+                var writeString = function (str) {
+                    for (var i = 0; i < str.length; i++) {
+                        data.setUint8(offset + i, str.charCodeAt(i));
+                    }
+                };
+
+                // 资源交换文件标识符
+                writeString('RIFF'); offset += 4;
+                // 下个地址开始到文件尾总字节数,即文件大小-8
+                data.setUint32(offset, 36 + dataLength, true); offset += 4;
+                // WAV文件标志
+                writeString('WAVE'); offset += 4;
+                // 波形格式标志
+                writeString('fmt '); offset += 4;
+                // 过滤字节,一般为 0x10 = 16
+                data.setUint32(offset, 16, true); offset += 4;
+                // 格式类别 (PCM形式采样数据)
+                data.setUint16(offset, 1, true); offset += 2;
+                // 通道数
+                data.setUint16(offset, channelCount, true); offset += 2;
+                // 采样率,每秒样本数,表示每个通道的播放速度
+                data.setUint32(offset, sampleRate, true); offset += 4;
+                // 波形数据传输率 (每秒平均字节数) 单声道×每秒数据位数×每样本数据位/8
+                data.setUint32(offset, channelCount * sampleRate * (sampleBits / 8), true); offset += 4;
+                // 快数据调整数 采样一次占用字节数 单声道×每样本的数据位数/8
+                data.setUint16(offset, channelCount * (sampleBits / 8), true); offset += 2;
+                // 每样本数据位数
+                data.setUint16(offset, sampleBits, true); offset += 2;
+                // 数据标识符
+                writeString('data'); offset += 4;
+                // 采样数据总数,即数据总大小-44
+                data.setUint32(offset, dataLength, true); offset += 4;
+                // 写入采样数据
+                if (sampleBits === 8) {
+                    for (var i = 0; i < bytes.length; i++, offset++) {
+                        var s = Math.max(-1, Math.min(1, bytes[i]));
+                        var val = s < 0 ? s * 0x8000 : s * 0x7FFF;
+                        val = parseInt(255 / (65535 / (val + 32768)));
+                        data.setInt8(offset, val, true);
+                    }
+                } else {
+                    for (var i = 0; i < bytes.length; i++, offset += 2) {
+                        var s = Math.max(-1, Math.min(1, bytes[i]));
+                        data.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+                    }
+                }
+                console.log("这是录音文件",data)
+                return new Blob([data], { type: 'audio/wav' });
+            }
+        };
+Date.prototype.Format = function (fmt) { //author: meizz
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+        //开始录音
+        this.start = function () {
+            console.log("start了吗！！！");
+            audioInput.connect(recorder);
+            recorder.connect(context.destination);
+        };
+
+        //停止
+        this.stop = function () {
+            console.log("stop了吗！！！");
+            recorder.disconnect();
+            //清理缓存音频
+            console.log("stoped",audioData.buffer.length)
+
+
+        };
+
+        //获取音频文件
+        this.getBlob = function () {
+        console.log("getBlob了吗！！！");
+             var time=this.stop();
+             console.log("录音这么久",time);
+             return {
+             duration:time,
+             blob:audioData.encodeWAV(),
+            };
+        };
+
+        this.clear=function(){
+        console.log("clear了吗！！！");
+            audioData.buffer=[];
+        }
+
+        //回放
+        this.play = function (audio,blob) {
+            blob=blob||this.getBlob().blob;
+            audio.src = window.URL.createObjectURL(blob);
+        };
+
+
+
+        //音频采集
+        recorder.onaudioprocess = function (e) {
+            audioData.input(e.inputBuffer.getChannelData(0));
+            //record(e.inputBuffer.getChannelData(0));
+        };
+
+    };
+    //抛出异常
+    HZRecorder.throwError = function (message) {
+        console.error(message);
+    };
+    //是否支持录音
+    HZRecorder.canRecording =!!navigator.getUserMedia;
+    //获取录音机
+    HZRecorder.get = function (callback, config) {
+        console.log("录音开始了吗2.5")
+         HZRecorder.throwError=config&&config.error||HZRecorder.throwError;
+        if (callback) {
+            if (navigator.getUserMedia) {
+                navigator.getUserMedia(
+                    { audio: true } //只启用音频
+                    , function (stream) {
+                        var rec = new HZRecorder(stream, config);
+                        callback(rec);
+                        console.log("录音开始了吗2.7",rec)
+                    }
+                    , function (error) {
+                        switch (error.code || error.name) {
+                            case 'PERMISSION_DENIED':
+                            case 'PermissionDeniedError':
+                                HZRecorder.throwError('用户拒绝提供信息。');
+                                break;
+                            case 'NOT_SUPPORTED_ERROR':
+                            case 'NotSupportedError':
+                                HZRecorder.throwError('浏览器不支持录音功能。');
+                                break;
+                            case 'MANDATORY_UNSATISFIED_ERROR':
+                            case 'MandatoryUnsatisfiedError':
+                                HZRecorder.throwError('无法发现指定的硬件设备。');
+                                break;
+                            default:
+                                HZRecorder.throwError('无法打开麦克风。异常信息:' + (error.code || error.name));
+                                break;
+                        }
+                    });
+            } else {
+                HZRecorder.throwErr('浏览器不支持录音功能。'); return;
+            }
+        }
+
+    };
+    window.HZRecorder = HZRecorder;
+},
+
+
+    startRecording(recorder) {
+               console.log("录音开始了吗0")
+               if(this.recorder){
+                   console.log("录音开始了吗2",this.recorder)
+                   this.recorder.start();
+                   return;
+               }
+                var tmp_rec;
+                HZRecorder.get(function(rec) {
+                    // console.log("录音开始了吗3",rec)
+                    tmp_rec=rec;
+
+                });
+                let that=this;
+                setTimeout( function(){
+                        that.recorder=tmp_rec;
+                        that.recorder.start();
+                        console.log("录音开始了吗3.5",this.recorder)
+
+                    }, 500 );
+                //this.recorder='aaaa';
+                console.log("录音开始了吗4",this.recorder)
+    },
+    sendRecording(recorder){
+
+                console.log("录音结束了吗",this.recorder)
+
+                var data=this.recorder.getBlob();
+
+                console.log("data",data);
+                var save_link = document.createElementNS('http://www.w3.org/1999/xhtml', 'a')
+                save_link.href = URL.createObjectURL(data.blob);
+                var now=new Date;
+                save_link.download = now.Format("yyyyMMddhhmmss");
+                var ev = document.createEvent('MouseEvents');
+                ev.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+                save_link.dispatchEvent(ev);
+                console.log("这是我获得的录音吗",now.Format("yyyyMMddhhmmss"))
+                this.recorder.clear;
+            },
+
+
+
+    getStr(){
+              var now=new Date;
+              var str= now.toDateString();
+            },
+    stopRecord(){
+                this.recorder&&this.recorder.stop();
+            },
+
+
+
     //显示结果进度
     showProcess(){
       console.log("showprocess")
